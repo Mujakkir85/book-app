bc3c1483d40abfae8e7173d55d727026655e97d6
Mujakkir <mujakkirlucse@gmail.com>
2022-09-13 16:51:05 +0600

    fixed form

--- src/componnets/AddBlog/AddBlog.js
+++ src/componnets/AddBlog/AddBlog.js
@@ -1,35 +1,59 @@
 import React from 'react';
-import {TextField, Button, Container} from "@mui/material";
+import {TextField, Button, Container, Stack, Typography} from "@mui/material";
 
 const AddBlog = () => {
-    const handlesubmit=()=>{
+    const [blogdata, setBlogdata] = useState([]);
 
+    data: [...prevState.data,
+        {
+            fname: prevState.fname,
+            lname: prevState.lname,
+            email: prevState.email,
+            subject: prevState.subject
+        }]
+
+    const handleSubmit=(event)=>{
+        event.preventDefault();
+        
+        const authorName = event.target.name.value;
+        const bookName = event.target.bookName.value;
+        const description = event.target.description.value;
+        console.log(authorName, bookName, description)
+
+        event.target.name.value = '';
+        event.target.bookName.value = '';
+        event.target.description.value = '';
     }
     return (
         <Container sx={{mt: '20px'}}>
-            <form onSubmit={handlesubmit}>
+
+            <form onSubmit={handleSubmit} >
+                <Stack sx={{width: '50%', mx: 'auto'}} spacing={4} >
+                <Typography variant='h4' sx={{mx:'auto'}} >ADD A NEW BLOG</Typography>
                 <TextField
                     id="outlined-basic"
-                    label="Outlined"
+                    label="Author_Name"
                     variant="outlined"
                     required fullWidth
                     name='name'
                 />
                 <TextField
                     id="outlined-basic"
-                    label="Outlined"
+                    label="Book_Name"
                     variant="outlined"
                     required fullWidth
-                    name='bookname'
+                    name='bookName'
                 />
                 <TextField
                     id="outlined-basic"
-                    label="Outlined"
+                    label="Description"
                     variant="outlined"
                     required fullWidth
                     name='description'
+                    multiline rows={3}
                 />
-                <Button variant="contained">Submit</Button>
+                <Button type='submit' fullWidth variant="contained">Submit</Button>
+                </Stack>
             </form>
         </Container>
     );
