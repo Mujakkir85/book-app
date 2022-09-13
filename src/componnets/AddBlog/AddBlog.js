0e44c1c7c1f798ac080318b9fb0e84a873383dee
Mujakkir <mujakkirlucse@gmail.com>
2022-09-13 20:05:17 +0600

    add array

--- src/componnets/AddBlog/AddBlog.js
+++ src/componnets/AddBlog/AddBlog.js
@@ -1,35 +1,35 @@
 import React from 'react';
+import {useState} from "react";
 import {TextField, Button, Container, Stack, Typography} from "@mui/material";
 
 const AddBlog = () => {
-    const [blogdata, setBlogdata] = useState({
-        authorName: '', bookName: '', description: ''
-    });
-
+    const [blogdata, setBlogdata] = useState([]);
     let authorName, bookName, description
-    let name, value;
-    const handleinput = (e) =>{
-        name = e.target.name;
-        value = e.target.value;
+
+    const handleSubmit=(event)=>{
+         event.preventDefault();
+        let authorName
+        let bookName
+        let description
+         const newdata = {
+             authorName: event.target.authorName.value,
+             bookName: event.target.bookName.value,
+             description: event.target.description.value,
+         }
+         setBlogdata([...blogdata, newdata]);
+        authorName = '';
+        bookName = '';
+        description = '';
+         console.log(authorName, bookName, description)
     }
 
-    // const handleSubmit=(event)=>{
-    //      event.preventDefault();
-    //      authorName = event.target.name.value;
-    //      bookName = event.target.bookName.value;
-    //      description = event.target.description.value;
-    //      console.log(authorName, bookName, description)
-    //
-    //      event.target.name.value = '';
-    //      event.target.bookName.value = '';
-    //      event.target.description.value = '';
-    // }
 
+    console.log(blogdata)
 
     return (
         <Container sx={{mt: '20px'}}>
 
-            <form onSubmit={handleSubmit} >
+            <form  onSubmit={handleSubmit} >
                 <Stack sx={{width: '50%', mx: 'auto'}} spacing={4} >
                 <Typography variant='h4' sx={{mx:'auto'}} >ADD A NEW BLOG</Typography>
                 <TextField
@@ -37,16 +37,14 @@ const AddBlog = () => {
                     label="Author_Name"
                     variant="outlined"
                     required fullWidth
-                    value={blogdata.authorName}
-                    onChange={handleinput}
+                    name='authorName'
                 />
                 <TextField
                     id="outlined-basic"
                     label="Book_Name"
                     variant="outlined"
                     required fullWidth
-                    value={blogdata.bookName}
-                    onChange={handleinput}
+                    name='bookName'
                 />
                 <TextField
                     id="outlined-basic"
@@ -55,8 +53,6 @@ const AddBlog = () => {
                     required fullWidth
                     name='description'
                     multiline rows={3}
-                    value={blogdata.description}
-                    onChange={handleinput}
                 />
                 <Button type='submit' fullWidth variant="contained">Submit</Button>
                 </Stack>
